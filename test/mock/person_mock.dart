0fe91031ee46013fef2333301552ecd7c18d0152
iury0393 <iury0393@gmail.com>
2021-07-28 14:45:43 -0300

    Adicionando teste do repositorio

--- test/mock/person_mock.dart
+++
@@ -1,4 +0,0 @@
-import 'package:http/http.dart';
-import 'package:mockito/mockito.dart';
-
-class PersonMock extends Mock implements Client {}
---
+++ test/person_mock.dart
@@ -0,0 +1,4 @@
+import 'package:http/http.dart' as http;
+import 'package:mockito/mockito.dart';
+
+class ClientMock extends Mock implements http.Client {}
